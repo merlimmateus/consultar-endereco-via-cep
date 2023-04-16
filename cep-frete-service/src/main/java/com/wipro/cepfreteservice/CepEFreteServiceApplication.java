e03c99899cb26687ed0255fa18367237631472c5
Mateus Merlim Mattos <merlimmateus@gmail.com>
2023-04-16 19:29:48 -0300

    Iniciando projeto spring boot java 11

---
+++ cep-frete-service/src/main/java/com/wipro/cepfreteservice/CepEFreteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.cepfreteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepEFreteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepEFreteServiceApplication.class, args);
+	}
+
+}
