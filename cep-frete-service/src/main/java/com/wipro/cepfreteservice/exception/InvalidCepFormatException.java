bced797fac5a33b8eff608f83592eafdf6d6eba5
Mateus Merlim Mattos <merlimmateus@gmail.com>
2023-04-17 01:39:19 -0300

    Revert "Criação de DockerFile e docker-compose.yml"

--- cep-frete-service/src/main/java/com/wipro/cepfreteservice/exception/InvalidCepFormatException.java
+++
@@ -1,7 +0,0 @@
-package com.wipro.cepfreteservice.exception;
-
-public class InvalidCepFormatException extends RuntimeException {
-    public InvalidCepFormatException(String message) {
-        super(message);
-    }
-}
