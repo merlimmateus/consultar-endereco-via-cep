0b7f71240cb32742b028e69d8261e4e128adf1c7
Mateus Merlim Mattos <merlimmateus@gmail.com>
2023-04-17 00:48:01 -0300

    Conserto de bug Unknow properties e melhora no tratamento de erros

--- cep-frete-service/src/main/java/com/wipro/cepfreteservice/model/Endereco.java
+++ cep-frete-service/src/main/java/com/wipro/cepfreteservice/model/Endereco.java
@@ -1,6 +1,7 @@
 package com.wipro.cepfreteservice.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Endereco {
 
     @JsonProperty("cep")
